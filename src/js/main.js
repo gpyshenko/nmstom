c85475242c835690a6feeec2efdff484e8feaf6a
gpyshenko <gpyshenko@gmail.com>
2018-04-12 01:43:28 +0500

    В разработке шапка и футер.

--- src/js/main.js
+++ src/js/main.js
@@ -1,58 +1,7 @@
 $(document).ready(function () {
 // Кнопка меню
     $('.js-burger').click(function () {
-        $('.js-sidebar').toggleClass('opened');
-        $('.shadow').fadeIn();
-    });
-    $('.shadow').click(function () {
-        $('.js-sidebar').toggleClass('opened');
-        $('.shadow').fadeOut();
-    });
-
-    var asideScroll = document.querySelector('.js-sidebar');
-    SimpleScrollbar.initEl(asideScroll);
-    // Слайдер Новостей
-    var newsSlider = $('.js-newsSlider');
-    newsSlider.slick({
-        arrows: false,
-        dots: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        variableWidth: true
-    });
-
-    $('.js-newsSliderPrev').click(function () {
-        newsSlider.slick('slickPrev');
-    });
-    $('.js-newsSliderNext').click(function () {
-        newsSlider.slick('slickNext');
-    });
-
-    // Слайдер мероприятий
-    var eventsSlider = $('.js-eventsSlider');
-    eventsSlider.slick({
-        arrows: false,
-        dots: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        variableWidth: true
-    });
-
-    $('.js-eventsSliderPrev').click(function () {
-        eventsSlider.slick('slickPrev');
-    });
-    $('.js-eventsSliderNext').click(function () {
-        eventsSlider.slick('slickNext');
-    });
-
-    // Слайдер партнеров
-    var sliderPartners = $('.js-sliderPartners');
-    sliderPartners.slick({
-        arrows: false,
-        dots: false,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        variableWidth: true
+        $('.js-menu').fadeToggle();
     });
 });
 
